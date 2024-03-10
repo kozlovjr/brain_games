67c71683891921bff254f344d700fbfddfbe60bf
nikitakozlovjr <nikita3726898@gmail.com>
2024-03-10 23:37:34 +0300

    [feat] add brain-even script

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from ..games.even import even_game
+from ..games.even import description
+from ..main import main
+
+if __name__ == '__main__':
+    main(description, even_game)
