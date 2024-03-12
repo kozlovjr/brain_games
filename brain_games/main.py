8ac31aede9b0c1880f3843ca45bc8297a4390e19
nikitakozlovjr <nikita3726898@gmail.com>
2024-03-12 18:09:57 +0300

    [refactor] transfer generate random num to game logic

--- brain_games/main.py
+++ brain_games/main.py
@@ -1,5 +1,4 @@
 from cli import welcome_user
-from get_random_num import get_random_num
 
 def main(description, round):
     print("Welcome to the Brain Games!")
@@ -11,10 +10,7 @@ def main(description, round):
     count_round = 1
 
     while count_round <= 3:
-        random_num = get_random_num()
-        print(f"Question: {random_num}")
-
-        [correct_answer, user_answer] = round(random_num)
+        [correct_answer, user_answer] = round()
 
         if correct_answer != user_answer:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
