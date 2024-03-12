e36e82baa4ff865fdd583cfee70359b554e25249
nikitakozlovjr <nikita3726898@gmail.com>
2024-03-12 18:08:53 +0300

    [refactor] add the executable file

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from ..games.calc import game_calc
+from ..games.calc import description
+from ..main import main
+
+if __name__ == '__main__':
+    main(description, game_calc)
