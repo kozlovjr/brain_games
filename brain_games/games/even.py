f3b42cd36f8e54495c6c31dc0bb6f501f80358d7
nikitakozlovjr <nikita3726898@gmail.com>
2024-03-12 18:03:21 +0300

    [redactor] transfer generate random num to game logic

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,8 +1,11 @@
 import prompt
+from ..get_random_num import get_random_num
 
 description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-def even_game(num):
+def even_game():
+    num = get_random_num()
+    print(f"Question: {num}")
     correct_answer = 'yes' if num % 2 == 0 else 'no'
     user_answer = prompt.string('Your answer: ')
 
